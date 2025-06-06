b57db3acf655ccb63e1703082857eaa071d4e46b
Ilya M <ilkafa93@yandex.ru>
2025-06-07 00:00:28 +0300

    Refactor infra

--- infra/gateway-server/src/main/java/ru/practicum/GatewayApp.java
+++ infra/gateway-server/src/main/java/ru/practicum/GatewayApp.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum;
+package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class Gateway {
+public class GatewayApp {
     public static void main(String[] args) {
-        SpringApplication.run(Gateway.class, args);
+        SpringApplication.run(GatewayApp.class, args);
     }
 }
-
